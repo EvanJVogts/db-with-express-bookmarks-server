3fcce4d9b5a18ad4fdbe4a998416255ceb53751c
Evan Vogts <evanjvogts@gmail.com>
2019-11-08 16:39:48 -0600

    finished

---
+++ src/app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+const { NODE_ENV } = require('./config');
+const bookmarkRouter = require('./bookmark/bookmark-router');
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
+app.use(bookmarkRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello from my bookmarks API!');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
